5584964b77e7d2d246dab54afef2ff14fcd28ef4
Tran Quang Huy <huykon225@gmail.com>
2023-03-24 16:36:19 +0700

    feat: initial pw

---
+++ tests/example.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
+
+test.describe("navigation", () => {
+  test.beforeEach(async ({ page }) => {
+    // Go to the starting url before each test.
+    await page.goto("https://www.dotlife.store/");
+  });
+
+  test('menu link', async ({ page }) => {
+
+    // click to the menu
+    await page.getByRole('link', { name: 'Category: SmartWatch. 8 sub-categories' }).click();
+
+    //expects the url
+    await expect(page).toHaveURL(/smartwatch.html/)
+  })
+});
